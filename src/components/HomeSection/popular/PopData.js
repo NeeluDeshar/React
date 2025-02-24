0fb5b351fb037fb9c714465782a76128b032fde9
Nirjala Deshar <nirjaladeshar98@gmail.com>
2025-02-24 15:18:07 +0545

    nepali ma rakheko xa room ko price

--- src/components/HomeSection/popular/PopData.js
+++ src/components/HomeSection/popular/PopData.js
@@ -1,33 +1,33 @@
 const PopularData = [
-    {
-      id: 1,
-      image: "images/room3.jpg",
-      country: "Deluxe",
-      name: "Deluxe Room",
-      price: "$300.00",
-    },
-    {
-      id: 2,
-      image: "images/room1.jpg",
-      country: "Single",
-      name: "Single Room",
-      price: "$400.00",
-    },
-    {
-      id: 3,
-      image: "images/room2.jpg",
-      country: "Suite",
-      name: "Homely Suite",
-      price: "$500.00",
-    },
-    {
-      id: 4,
-      image: "images/room3.jpg",
-      country: "Double Room",
-      name: "Ansan Hotel",
-      price: "$100.00",
-    },
-    
-  ]
-  
-  export default PopularData
+  {
+    id: 1,
+    image: "images/room3.jpg",
+    country: "Deluxe",
+    name: "Deluxe Room",
+    price: "Rs.3000.00",
+  },
+  {
+    id: 2,
+    image: "images/room1.jpg",
+    country: "Single",
+    name: "Single Room",
+    price: "Rs.2500.00",
+  },
+  {
+    id: 3,
+    image: "images/room2.jpg",
+    country: "Suite",
+    name: "Homely Suite",
+    price: "Rs. 3500.00",
+  },
+  {
+    id: 4,
+    image: "images/room3.jpg",
+    country: "Double Room",
+    name: "Ansan Hotel",
+    price: "Rs. 5000.00",
+  },
+
+]
+
+export default PopularData
--- src/components/Rooms/Tdata.js
+++ src/components/Rooms/Tdata.js
@@ -4,30 +4,30 @@ const Tdata = [
     image: "images/room3.jpg",
     room: "Deluxe",
     name: "Deluxe Room",
-    price: "$300.00",
+    price: "Rs. 3000.00",
   },
   {
     id: 2,
     image: "images/room1.jpg",
     room: "Single",
     name: "Single Room",
-    price: "$400.00",
+    price: "Rs. 2500.00",
   },
   {
     id: 3,
     image: "images/room2.jpg",
     room: "Suite",
     name: "Homely Suite",
-    price: "$500.00",
+    price: "Rs. 3500.00",
   },
   {
     id: 4,
     image: "images/room3.jpg",
     room: "Double Room",
     name: "Droom",
-    price: "$100.00",
+    price: "Rs. 5000.00",
   },
-  
+
 ]
 
 export default Tdata
